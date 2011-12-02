d0bf9836be73430157d602c0672d0b5455103b59
Chris Matthews <chris4000@gmail.com>
2011-12-02 15:05:04 -0800

    added in special code for compoent request handling.

--- sysdeps/nacl/ioctl.c
+++ sysdeps/nacl/ioctl.c
@@ -23,19 +23,30 @@ __ioctl (int fd, unsigned long int request, ...)
 {
   nacl_strace("ioctl");
 
-  int result;
+  int result = -1;
+
   if (is_system_handle(fd)) {
     /* I think system handles can't have ioctls because they are not implemented
      * in NaCl, so we should return ENOSYS on error no and fail for this handle  */
     result = -38;
-  }
-  else{
-    /* send to lind server. For now, don't send optional args  */
-    result = lind_ioctl_rpc(fd, request);
+  } else {
+    if (request > 10000 && request <= 10004) {
+      /* Do a lind component call */
+        nacl_strace("comp ioctl");
+
+        va_list argp;
+	va_start(argp, request);
+	result = lind_comp_rpc(request, va_arg(argp,int), va_arg(argp, void*));
+	va_end(argp);
+      
+    } else {
+      /* send to lind server. For now, don't send optional args  */
+      result = lind_ioctl_rpc(fd, request);
+    }
   }
     
   if (result < 0) {
-    errno = -result;
+    _set_errno(-result);
     return -1;
   }
   return 0;
