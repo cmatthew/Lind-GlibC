72e9ba2df80577d0848e536a4e833877e7ba5ed7
Chris Matthews <chris4000@gmail.com>
2011-12-02 15:00:27 -0800

    updated version of getpid

--- sysdeps/nacl/getpid.c
+++ sysdeps/nacl/getpid.c
@@ -3,16 +3,17 @@
 #include <nacl_syscalls.h>
 #include "nacl_util.h"
 #include "strace.h"
-#include "lind_rpc.h"
+#include "lind_syscalls.h"
 
 pid_t __getpid ()
 {
   pid_t lind_pid = -1;
   nacl_strace("getpid");
 
-  const lind_rpc_status rc = depricated_nacl_rpc_syscall(NACL_sys_getpid, "", 0, NULL, &lind_pid, 0);
-  if (rc != RPC_OK) {
-    nacl_strace("getpid - failed!");
+  int rc = lind_getpid_rpc(&lind_pid);
+
+  if (rc != 0) {
+    __set_errno(rc);
     return -1;
   }
   return lind_pid;
