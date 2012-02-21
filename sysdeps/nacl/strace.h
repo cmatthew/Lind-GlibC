6b8eda9b9d700dd23de6a43cf2643f84773d147d
Chris Matthews <chris4000@gmail.com>
2012-02-20 16:23:09 -0800

    By default, turn off tracing.

--- sysdeps/nacl/strace.h
+++ sysdeps/nacl/strace.h
@@ -7,10 +7,11 @@ int get_logging_status(void);
 void set_ready_to_log(void);
 
 void set_no_logging(void); 
+
 void _lind_strace(const char* syscall);
 
 
-#define USE_TRACE 1 
+/* #define USE_TRACE  */
 
 #ifdef USE_TRACE
 #define nacl_strace(x) _lind_strace(x)
