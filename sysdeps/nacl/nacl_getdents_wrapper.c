03d6b1c6072b60a8deada6673c9883b298ab074f
Chris Matthews <chris4000@gmail.com>
2012-02-20 16:26:40 -0800

    working getdents.

--- sysdeps/nacl/nacl_getdents_wrapper.c
+++ sysdeps/nacl/nacl_getdents_wrapper.c
@@ -5,16 +5,15 @@
 /* The purpose of this file is to be #included by generic readdir
    implementations.  */
 
-static const int d_name_shift = offsetof (DIRENT_TYPE, d_name) -
-    offsetof (struct nacl_abi_dirent, nacl_abi_d_name);
+static const int d_name_shift = offsetof (DIRENT_TYPE, d_name) - offsetof (struct nacl_abi_dirent, nacl_abi_d_name);
 
 /* Calls NACL_SYSCALL(getdents) and converts resulting buffer to glibc abi.
    This wrapper is required since glibc abi for DIRENT_TYPE differs from
    struct nacl_abi_dirent. */
 __ssize_t internal_function __GETDENTS (int fd, char *buf, size_t buf_size)
 {
-  nacl_strace("getdents");
-  ssize_t result = lind_getdents_rpc(fd, buf, buf_size);
-  return result;
+  int result = lind_getdents_rpc(fd, buf_size, buf);
+
+  return (ssize_t) result;
 
 }
