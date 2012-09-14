dab7b47e11e54b49656dc2e6490d1a0df761583e
Chris Matthews <chris4000@gmail.com>
2012-09-14 15:43:11 -0700

    default to no printing cleanup of options

--- sysdeps/nacl/strace.h
+++ sysdeps/nacl/strace.h
@@ -17,17 +17,17 @@ ssize_t __write(int desc, void const *buf, size_t count);
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
-#define dbp(x)   {__write(2, __FILE__ ":" TOSTRING(__LINE__) ": " x "\n", strlen( __FILE__ ":" TOSTRING(__LINE__) ": "  x "\n")); }
-#define dbg_print(x)   { const char * dbg_message = (x);__write(2, dbg_message, strlen(dbg_message)); }
-//#define dbg_print(x)    {int xyz = 0;} 
 
-
-// #define USE_TRACE
+//#define USE_TRACE
 
 #ifdef USE_TRACE
-#define nacl_strace(x) _lind_strace(x)
+  #define nacl_strace(x) _lind_strace(x)
+  #define dbp(x)   {__write(2, __FILE__ ":" TOSTRING(__LINE__) ": " x "\n", strlen( __FILE__ ":" TOSTRING(__LINE__) ": "  x "\n")); }
+  #define dbg_print(x)   { const char * dbg_message = (x);__write(2, dbg_message, strlen(dbg_message)); }
 #else
-#define nacl_strace(x) 
+  #define nacl_strace(x) 
+  #define dbp(x)    {;} 
+  #define dbg_print(x)    {;} 
 #endif
 
 #define print_error(x) _lind_strace(x)
